261f70f52ae3f32f5526844e1f7d2dbf9ed754a0
Bookervsky <booklv123@gmail.com>
2026-01-16 12:20:02 +0800

    chg: define main.py as the entrypoint to run instances

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+import logging
+
+from src.network import Node, Link, Network
+from src.DNDP import DNDP
+from src.BC import BC
+
+
+if __name__ == "__main__":
+    start = time.time()
+    dndp = BC(
+        work_dir="data/LangFang/",
+        node_file="LangFang_nodes.csv",
+        link_file="LangFang_net.csv",
+        OD_file="morning_peak_OD.npy",
+    )
+    dndp.run_workflow()
+    end = time.time()
+    logging.info(f"Total running time: {end - start} seconds")
